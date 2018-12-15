3aacd034371e063ee0d512431b6eba09dc5d529d
Nora Bonifas <nbonifas@luc.edu>
2018-12-15 17:30:28 -0600

    Added my login screens

---
+++ www/js/loginpage.js
@@ -0,0 +1,19 @@
+// const firebaseConfig -={
+
+//  };
+
+//function fireBaseInit(){
+  //var networkState = navigator.connection.type;
+  //var loginBtn = document.getElementByID;
+  //var signupBtn = document.getElementByID("signupBtn");
+  //var logoutBtn = document.getElementById("logoutBtn");
+  //if (networkState !== Connectoin.None) {
+    //firebase.initializeApp(firebaseConfig);
+
+    //const db = firebase.database();
+    //var user = firebase.auth().currentUser;
+    //console.log(db);
+    //const dbRoot = db.ref("users");
+
+//  }
+//}
